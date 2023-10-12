eb49796d1ac7b4b13f63bf78cb451068b6745f40
AngieN7 <117302924+AngieN7@users.noreply.github.com>
2023-10-12 12:24:02 -0400

    Add files via upload
    Drafts of working code for some of PPL1-PPL4, still working on things, though.

---
+++ bool_demo.rs
@@ -0,0 +1,8 @@
+//Creating bool varible and using logic opporations
+fn main(){
+    //Creating Bool variable 
+    let t: bool = true;
+    //Example of bool with operator
+    let f: bool = !t;//!n oporator negates bools so f is the negation of t.
+    println!("{}", f);//prints false 
+}
---
+++ int_float.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let x: i32 = 5;
+    let y: i32  = 11;
+    let xy=x+y;
+    println!("{xy}");
+}
